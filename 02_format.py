b5226f61f6629260394aa17857086b6ba9fe230b
Emmanuel Estrada <emmanuelre93@gmail.com>
2022-12-27 21:51:10 -0600

    Prueba de commit despues de crear llave ssh

--- 02_format.py
+++ 02_format.py
@@ -8,3 +8,4 @@ print('Mi nombre es "Emmanuel" ¿y el tuyo?')
 print('Hola, mi nombre es ' + name + ' y mi apellido es ' + lastname)
 print('Hola, mi nombre es {} y mi apellido es {}'.format(name, lastname))
 print(f'Hola, mi nombre es {name} y mi apellido es {lastname}')
+#prueba de commit
